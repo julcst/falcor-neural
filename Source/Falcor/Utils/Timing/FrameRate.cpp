6dfc3f9a78cb0b59bc66cbfd504301cd585b51f1
Julcs <mail@julcs.com>
2026-04-26 20:40:11 +0200

    Higher frametime resolution

--- Source/Falcor/Utils/Timing/FrameRate.cpp
+++ Source/Falcor/Utils/Timing/FrameRate.cpp
@@ -33,7 +33,7 @@ namespace Falcor
 std::string FrameRate::getMsg(bool vsyncOn) const
 {
     double frameTime = getAverageFrameTime();
-    std::string msg = fmt::format("{:.1f} FPS ({:.1f} ms/frame)", 1.f / frameTime, frameTime * 1000.0);
+    std::string msg = fmt::format("{:.1f} FPS ({:.3f} ms/frame)", 1.f / frameTime, frameTime * 1000.0);
     if (vsyncOn)
         msg += std::string(", VSync");
     return msg;
